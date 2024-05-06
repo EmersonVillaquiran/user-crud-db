14f7f50aba0d86a46504c41415897294083cab9e
CamiloVillaquiran <112994996+EmersonVillaquiran@users.noreply.github.com>
2024-05-06 18:51:32 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const userRouter = require('./user.router')
 
 // colocar las rutas aquí
 
 
-module.exports = router;
+module.exports = userRouter;
